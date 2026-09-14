80334ea9dea32568114914b661c5eabca2ae8ce6
Thorsten Naumann <thosten.naumann@bergfreunde.de>
2026-09-14 11:33:59 +0200

    Revert "feat(app): color Boc chat markdown"
    This reverts commit 409afdbfc98636aefebbeb08b0557646af2119be.

--- packages/app/src/boc/product.ts
+++ packages/app/src/boc/product.ts
@@ -1,12 +1,6 @@
 import { createBocTranslator } from "@boc/extensions/renderer"
-import "./chat-colors.css"
 
 export const bocProduct = import.meta.env.VITE_BOC_PRODUCT
-
-if (typeof document !== "undefined") {
-  if (bocProduct) document.documentElement.dataset.bocProduct = bocProduct
-  if (!bocProduct) delete document.documentElement.dataset.bocProduct
-}
 
 export function bocProductPresentation(locale: () => string) {
   if (!bocProduct) return undefined
